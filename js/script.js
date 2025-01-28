3c7f3f92b1956d5af4ea107352c969c54220c20a
Moussa Sagna <sagnamoussa546@gmail.com>
2025-01-28 12:24:56 +0000

    Initialisation du projet : structure de base HTML, CSS et JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("addTask").addEventListener("click", function () {
+  let task = document.getElementById("taskInput").value;
+  if (task) {
+    let li = document.createElement("li");
+    li.textContent = task;
+    document.getElementById("taskList").appendChild(li);
+    document.getElementById("taskInput").value = "";
+  }
+});
